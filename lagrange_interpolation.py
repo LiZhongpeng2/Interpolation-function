922fb0da675d3bd30612f3400cdc5ad976e840d1
LiZhongpeng2 <lizhongpeng2@gmail.com>
2025-05-06 19:34:32 +0800

    result visible

--- lagrange_interpolation.py
+++ lagrange_interpolation.py
@@ -1,5 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+
+#using Chinese font
+plt.rcParams['font.family'] = 'SimHei'
 
 #数据点
 #x从0^2到8^2
@@ -15,12 +19,12 @@ def lagrange_interpolation(x_data, y_data):
     #先拿到数组的大小，用于控制Ln的次数
     n = len(x_data)
 
-    #初始化x点处的插值结果result = 0
-    result = 0
-
     #定义一个函数，用于构造
     #传入x并返回该点处的插值结果
     def lagrange_function(x):
+
+        #初始化x点处的插值结果result = 0
+        result = 0
 
         #第一层控制的是Ln的n
         for i in range(n):
@@ -34,11 +38,42 @@ def lagrange_interpolation(x_data, y_data):
 
             #将每一项的Ln加入最终结果
             result += Ln_value
-    return result
+        return result
+    return lagrange_function #return this function so can be used later
+
 
 #创建题目的L8
 #L8(x)接受一个x值并返回x处的插值结果 例如L8(1)
 L8 = lagrange_interpolation(x, y)
+
+#----verify process----
+save_dir = 'verify_results/lagrange'
+#exist_ok=True means if this directory already exists, it will not be overwritten
+os.makedirs(save_dir, exist_ok=True)
+
+#integer in range 0-64
+x_integer = np.arange(0,65)
+y_lagrange = np.array([L8(x) for x in x_integer])
+y_true = np.sqrt(x_integer)
+
+#draw the plot
+plt.figure(figsize=(12,6))
+plt.plot(x_integer, y_lagrange, color='blue', label='拉格朗日插值')
+plt.plot(x_integer, y_true, color='red', label='真实值')
+
+plt.title('拉格朗日插值 VS 真实值')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend()
+plt.grid(True)
+
+#save the plot
+plt.savefig(os.path.join(save_dir, 'lagrange_interpolation.png'))
+
+
+
+
+
 
 
 
